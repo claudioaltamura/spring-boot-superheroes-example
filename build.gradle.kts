2fa0b18d3908af1de159e31379ed9002184a93b7
claudioaltamura <claudio@claudioaltamura.de>
2024-12-20 17:31:42 +0100

    build: upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     id ("java")
     id("com.diffplug.spotless") version "6.25.0"
     id("com.github.ben-manes.versions") version "0.51.0"
-    id("org.springframework.boot") version "3.2.2"
-    id("org.springdoc.openapi-gradle-plugin") version "1.8.0"
-	id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.4.1"
+    id("org.springdoc.openapi-gradle-plugin") version "1.9.0"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "de.claudioaltamura"
@@ -27,7 +27,7 @@ dependencies {
 	implementation ("org.springframework.boot:spring-boot-starter-web")
     implementation ("org.springframework.boot:spring-boot-starter-actuator")
     developmentOnly ("org.springframework.boot:spring-boot-devtools")
-    implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
 	runtimeOnly ("com.h2database:h2")
     testCompileOnly ("org.projectlombok:lombok")
     testAnnotationProcessor ("org.projectlombok:lombok")
